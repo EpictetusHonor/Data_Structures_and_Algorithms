06d5cd69baa28e581f6d4c1467af54588b2bfdde
tomasagustinrossi <diiucapri@gmail.com>
2025-08-06 21:58:55 -0300

    Create Two_Maximum_Minimum.pas

---
+++ Two_Maximum_Minimum.pas
@@ -0,0 +1,56 @@
+program Two_Maximum_Minimum;
+type
+  maximums = record;
+    max1:integer;
+    max2:integer;
+  end;
+procedure maximum(var m:maximums;num:integer);
+begin
+  if num >= m.max1 then
+  begin
+    m.max2:=m.max1;
+    m.max1:=num;
+  end
+  else
+  begin
+      	if num >= m.max2 then
+          m.max2:=num;
+  end;
+  maximums:=m;
+end;
+var
+  max:maximums;
+  number,count:integer;
+begin
+  max.max1:=-32768;
+  max.max2:=-32768;
+  writeln('------"Maximum/minimum program"------');
+  writeln('Write a number: (to stop or don"t inicialize the loop write the number: -32768)');
+  readln(number);
+  count:=0;
+  while (number<>-32768) do
+  begin
+    maximum(max,number);
+    readln(number);
+    count:=count+1;
+  end;
+  
+  if (count>=2) then
+  begin
+    writeln('The maximums numbers are:);
+    writeln('First: ', max.max1);
+    writeln('Second: ', max.max2);
+  end
+  else
+  begin
+    if (count=0) then
+    begin
+      writeln('Please don’t hesitate to reopen the program whenever you need it.');
+    end
+    else
+    begin
+      if (count=1) then
+        writeln('You putted only one number.);
+    end;
+  end;
+end;
