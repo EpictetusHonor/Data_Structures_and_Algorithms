4fe0a47d3eb4756f6fd340c260f05ef76947160c
TomasAgustinRossi <diiucapri@gmail.com>
2025-08-08 18:11:39 -0300

    Update Binary_tree_traversal.pas

--- Binary_tree_traversal.pas
+++ Binary_tree_traversal.pas
@@ -1,47 +1,64 @@
-program arboles;
+program tree;
 type
-	arbol = ^nodo;
-	nodo = record
-		HijoIz:arbol;
-		HijoDer:arbol;
+	tree = ^node;
+	node = record
+ 		data:integer;
+		cl:tree;
+		cr:tree;
 		end;
-var
-	a:arbol;
-	num:integer;
-
-//procedure preorden(a:arbol);
-//begin
-	//if (a<>nil) then
-	//begin
-		//writeln(a^.data);
-		//preorden(a^.hi);
-		//preorden(a^.hd);
-	//end;
-//end;
-//procedure postorden(a:arbol);
-//begin
-	//if (a<>nil) then
-	//begin
-		//postorden(a^.hi);
-		//postorden(a^.hd);
-		//writeln(a^.data);
-	//end;
-//end;
-
-procedure enOrden(a:arbol);
+procedure add(var t:tree;n:integer);
 begin
-	if (a<>nil) then begin
-		enOrden(a^.hi);
-		writeln(a^.dato);
-		enOrden(a^.hd);
+	if (t = nil) then
+	begin
+		new(t);
+		t^.data:=n;
+		t^.cl:=nil;
+		t^.cr:=nil;
+	end
+	else
+	begin
+		if (t^.data<=n) then
+			add(t^.cl,n);
+		else
+			agregar(t^.cr,n);
 	end;
 end;
+//procedure preorder(t:tree);
+//begin
+	//if (t<>nil) then
+	//begin
+		//writeln(t^.data);
+		//preorder(t^.cl);
+		//preorder(t^.cr);
+	//end;
+//end;
+//procedure postorder(t:treee);
+//begin
+	//if (t<>nil) then
+	//begin
+		//postorden(t^.cl);
+		//postorden(t^.cr);
+		//writeln(t^.data);
+	//end;
+//end;
+
+procedure enOrden(t:tree);
 begin
-	a:=nil;
+	if (t<>nil) then begin
+		enOrden(t^.cl);
+		writeln(t^.data);
+		enOrden(t^.cr);
+	end;
+end;
+var
+	trees:tree;
+ 	num:integer;
+begin
+	trees:=nil;
 	read(num);
 	while (num<>50) do
 	begin
-		agregar(a,num); //se dispone
+		add(trees,num);
 		read(num);
 	end;
 	enOrden(a);
